add971934a247dd918c81074c677e30ebbd4c550
emdeeeks <gareth+github@synbyte.com>
2018-12-13 23:07:42 +0000

    Making it comply with rubocop

--- lib/ozoned.rb
+++ lib/ozoned.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'optparse'
 require 'awesome_print'
 require 'yaml'
 require_relative 'ozoned/blockchain.rb'
 
-if File.exists? ("config.yml")
-  options = YAML.load_file("config.yml")
-else
-  options = {}
-end
+options = if File.exist? 'config.yml'
+            YAML.load_file('config.yml')
+          else
+            {}
+          end
 
 if ARGV.any?
   OptionParser.new do |op|
-    op.on('-h', '--help', 'This help message') do |help|
+    op.on('-h', '--help', 'This help message') do |_help|
       puts op
       exit
     end
-    op.on('-v', '--version', 'Print version and exit') do |version|
+    op.on('-v', '--version', 'Print version and exit') do |_version|
       options['network'] = 'testnet'
       exit
     end
