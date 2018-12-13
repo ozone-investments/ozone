f03947a5dbd5cbc5085b64df586cc14d8c5ef81e
emdeeeks <gareth+github@synbyte.com>
2018-12-13 22:36:22 +0000

    Making line smaller and also adding pending test

--- spec/ozoned/network_spec.rb
+++ spec/ozoned/network_spec.rb
@@ -3,9 +3,14 @@
 require_relative '../../lib/ozoned/network'
 
 RSpec.describe Network do
+  describe '.add_peer' do
+    it 'adds my IP to the peers list'
+  end
+
   describe '.my_ip' do
     it 'returns my IP address' do
-      expect(described_class.my_ip).to eq(Net::HTTP.get(URI('https://api.ipify.org')))
+      external_ip = Net::HTTP.get(URI('https://api.ipify.org'))
+      expect(described_class.my_ip).to eq(external_ip)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.fail_fast = true
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
